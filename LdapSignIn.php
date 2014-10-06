945acdbef0fd939063335c5950576e37f02ea9c0
marvinscharle <marvin.scharle@conclurer.com>
2014-10-06 17:06:44 +0200

    Initial commit

---
+++ LdapSignIn.php
@@ -0,0 +1,163 @@
+<?php
+
+class LdapSignIn extends WireData implements Module, ConfigurableModule
+{
+
+    protected $ldapSession = null;
+    protected $host, $port, $defaultLoginDomain, $userDefaultRoles;
+
+    public static function getModuleInfo()
+    {
+        return array(
+            'title' => __('LDAP Sign In'),
+            'version' => '050',
+            'author' => 'Conclurer GbR',
+            'summary' => __('Enables users to sign in via LDAP'),
+            'singular' => true,
+            'autoload' => true
+        );
+    }
+
+    public function ___install()
+    {
+        if (!function_exists('ldap_connect')) throw new WireException('Please make sure that extension php_ldap is loaded.');
+    }
+
+    public function init()
+    {
+        if (isset($this->data['host'])) {
+            $host = explode(':', $this->data['host']);
+            $this->host = $host[0];
+            $this->port = empty($host[1]) ? 389 : $host[1];
+        }
+
+        if (isset($this->data['loginDomain'])) $this->defaultLoginDomain = $this->data['loginDomain'];
+        $this->userDefaultRoles = new WireArray();
+
+        if (isset($this->data['userDefaultRoles'])) {
+            foreach ($this->data['userDefaultRoles'] as $x) {
+                $role = $this->roles->get($x);
+                $this->userDefaultRoles->add($role);
+            }
+        }
+
+        $this->userDefaultRoles->add($this->roles->getGuestRole());
+        $this->userDefaultRoles = $this->userDefaultRoles->unique();
+
+        $this->session->addHookAfter('login', $this, 'hookLogin');
+        $this->addHook('ProcessLogin::buildLoginForm', $this, 'hookLoginForm');
+    }
+
+    public function ___hookLogin(HookEvent $event)
+    {
+        if ($event->return) return; // Skip if already logged in
+
+        $username = $event->arguments[0];
+        $password = $event->arguments[1];
+
+        // skip if user already exists locally
+        if (!$this->users->get("name=$username") instanceof NullPage) return;
+
+        // Set default user domain name if not given
+        $username = str_replace('@', '', $username) == $username ? "$username@{$this->defaultLoginDomain}" : $username;
+
+        if ($this->ldapUserLogin($username, $password)) {
+            $wireUserName = $this->sanitizer->pageName($username);
+
+            $user = $this->users->get("name=$wireUserName");
+            if ($user instanceof NullPage) {
+                $usersPath = $this->users->getGuestUser()->parent;
+
+                $user = new User();
+                $user->parent = $usersPath;
+                $user->name = $wireUserName;
+                $user->pass = $password;
+                foreach ($this->userDefaultRoles as $role) $user->addRole($role);
+                $user->save();
+            } else {
+                $user->pass = $password;
+                $user->save();
+            }
+
+            // Reset given LDAP-Username to WireUsername
+            $user = $this->session->login($wireUserName, $password);
+            $event->return = $user;
+
+            $this->message($this->_('Logged in via LDAP'));
+
+            return;
+        }
+
+        $event->return = null;
+        return;
+    }
+
+    public function hookLoginForm(HookEvent $event)
+    {
+        $form = $event->return;
+        $defaultDomain = $this->defaultLoginDomain;
+        if (empty($defaultDomain)) return;
+
+        $bodyField = wire('modules')->get('InputfieldMarkup');
+        $bodyField->description = sprintf(__('You will be logged in via domain %1$s if you don\'t supply an different domain name'), "<b>$defaultDomain</b>");
+        $form->add($bodyField);
+
+        $event->return = $form;
+    }
+
+    public function ldapUserLogin($user, $password)
+    {
+        $connection = $this->connectLdap();
+        if (!$connection) return false;
+
+        $bind = @ldap_bind($connection, $user, $password);
+
+        return ($bind !== false);
+    }
+
+    public function validateConfiguration()
+    {
+        $connection = $this->connectLdap();
+        if (!$connection) {
+            $this->error($this->_('Unable to connect to LDAP server.'));
+            return;
+        }
+
+        $this->message($this->_('Successfully connected to LDAP server.'));
+    }
+
+    protected function connectLdap()
+    {
+        return ldap_connect($this->host, $this->port);
+    }
+
+    static public function getModuleConfigInputfields(array $data)
+    {
+        $inputfields = new InputfieldWrapper();
+
+        $hostField = wire('modules')->get('InputfieldText');
+        $hostField->name = 'host';
+        $hostField->label = __('LDAP Server');
+        if (isset($data['host'])) $hostField->value = $data['host'];
+        $hostField->description = __('The hostname of your LDAP server. This can be either an ip address or a domain name. Supply a custom port (different than 389) separated with a column. Examples: 10.0.0.1, controller.domain.com, controller.domain.com:388');
+        $inputfields->add($hostField);
+
+        $defaultLoginDomainField = wire('modules')->get('InputfieldText');
+        $defaultLoginDomainField->name = 'loginDomain';
+        $defaultLoginDomainField->label = __('Default Login Domain');
+        if (isset($data['loginDomain'])) $defaultLoginDomainField->value = $data['loginDomain'];
+        $defaultLoginDomainField->description = __('This is the domain name used by default if the user does not supply a domain name. It will be added to the username, e.g. username@domainname.com');
+        $inputfields->add($defaultLoginDomainField);
+
+        $userDefaultRolesField = wire('modules')->get('InputfieldPageListSelectMultiple');
+        $userDefaultRolesField->name = 'userDefaultRoles';
+        $userDefaultRolesField->label = __('Default Roles for new Users');
+        $userDefaultRolesField->description = __('These user roles will be applied to all new LDAP users. Please note that the guest role is applied automatically.');
+        $userDefaultRolesField->parent_id = wire('roles')->getGuestRole()->parent_id;
+        if (isset($data['userDefaultRoles'])) $userDefaultRolesField->value = $data['userDefaultRoles'];
+        $inputfields->add($userDefaultRolesField);
+
+        return $inputfields;
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+# LDAP for ProcessWire
+
+This is a module for ProcessWire that enables user authentication over LDAP, so you are able to use your Active Directory users.
+
+**Features**
+
+- Native ProcessWire Sign In 
+- Ability to specify a default domain name
+- Ability to specity a default role for new LDAP users
+- Passwords for LDAP users are automatically updated
+- Settings and messages are translatable
+
+**Login Page and Process**
+
+[<img alt="Login Page" src="http://abload.de/img/ldap-login-page2bi6y.png" height="300px">](http://abload.de/img/ldap-login-page2bi6y.png) [<img alt="Login Process" src="http://abload.de/img/ldap-login-successfult8dg1.png" height="300px">](http://abload.de/img/ldap-login-successfult8dg1.png) 
+
+**Module Settings**
+
+[<img alt="Module Settings" src="http://abload.de/img/ldap-settings-english5nc2i.png" height="400px">](http://abload.de/img/ldap-settings-english5nc2i.png)
+
+**Translated Module Settings**
+
+[<img alt="Module Settings" src="http://abload.de/img/ldap-settings-germannzdop.png" height="400px">](http://abload.de/img/ldap-settings-germannzdop.png)
+
+
+## Translation
+
+This module ships with a translation into German. To install it, just upload the json file - which you can find under /translations - to your defined language.
